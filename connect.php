d26f4eee8e1632c096a556a8b2ec58b3f0034c67
Sylvian Brunet <37698997+Sylvian-Brunet@users.noreply.github.com>
2019-12-09 16:09:34 +0100

    Version 0.2 - get content of peoples.json

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 
 function get_content(){
     $token = 'your_token';
-    $url = 'https://api.github.com/users/Sylvian-Brunet';
+    $url = 'https://api.github.com/repos/miw05/directory/contents/peoples.json';
     $curl = curl_init();
     $headers = Array('Authorization: Bearer ' . $token);
     curl_setopt($curl, CURLOPT_URL, $url);
@@ -13,10 +13,12 @@ function get_content(){
     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
     $data = curl_exec($curl);
-    var_dump(json_decode($data));
+    $json = json_decode($data, true);
+    $decode = base64_decode($json['content']);
+    var_dump(json_decode($decode));
+    
     curl_close ($curl);
 }
 
 get_content();
-
 ?>
